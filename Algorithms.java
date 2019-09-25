a0ab1e2f5fbcb11fe6fd6fe8f4fd0dac58a896e2
Nathan Lervold <C21nathan.lervold@usafa.edu>
2019-09-25 07:13:16 -0600

    added FloydWarshall to algorithms

--- Algorithms.java
+++ Algorithms.java
@@ -1,8 +1,69 @@
-package VisualizationTool_Project;
-
 public class Algorithms {
-    public static Graph algorithmName(Graph graph, Node source){
-        return null;
+
+    private static final double INF = Double.POSITIVE_INFINITY;
+
+    public static double[][] FloydWarshall(Graph graph){
+        //make adjacency matrix
+        double matrix[][] = new double[graph.getNumNodes()][];
+
+        //Initialize matrix distances
+        for (int i = 0; i < graph.getNumNodes(); ++i) {
+            for (int j = 0; j < graph.getNumNodes(); ++j) {
+                matrix[i][j] = INF;
+            }
+            System.out.print("\n");
+        }
+
+        //fill matrix with graph data
+        for(int i = 0; i < graph.getNumNodes(); ++i) {
+            for (int j = 0; j < graph.getNumLinks(); ++j) {
+                if(i == graph.getLinks().get(j).getNode1().getId()){
+                    matrix[i][graph.getLinks().get(j).getNode2().getId()] = graph.getLinks().get(j).getValue();
+                }
+            }
+        }
+        //print adjacency matrix for testing purposes
+        printMatrix(matrix, graph.getNumNodes());
+
+        int V = matrix.length;
+        double dist[][] = new double[V][V]; //output matrix
+
+        //initialize output matrix as input matrix
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                dist[i][j] = matrix[i][j];
+            }
+        }
+
+        //run core algorithm
+        for (int k = 0; k < V; ++k) {
+            for (int i = 0; i < V; ++i) {
+                for (int j = 0; j < V; ++j) {
+
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        //print distances matrix for testing and return dist for future use
+        printMatrix(dist, V);
+        return dist;
+    }
+
+    private static void printMatrix(double[][] matrix, int numNodes){
+        System.out.println("Shortest distance matrix:");
+        for (int i = 0; i < numNodes; ++i) {
+            for (int j = 0; j < numNodes; ++j) {
+                if (matrix[i][j] == INF) {
+                    System.out.print("INF\t");
+                } else {
+                    System.out.printf("%.1f\t", matrix[i][j]);
+                }
+            }
+            System.out.print("\n");
+        }
     }
 
 
@@ -49,11 +110,16 @@ public class Algorithms {
 
         }
 
-
         //distance should be an array of the shortest path from the source to each corresponding node on the graph
         return distance;
     }
 
 
 
+
+
+    public static Graph algorithmName(Graph graph, Node source){
+        return null;
+
+    }
 }
