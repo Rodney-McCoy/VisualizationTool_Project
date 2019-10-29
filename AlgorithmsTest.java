57b21869d6a534a2bf342cf847cfb226527cb4d1
17mccurdyt <54079454+17mccurdyt@users.noreply.github.com>
2019-10-29 12:31:26 -0600

    Add files via upload

--- AlgorithmsTest.java
+++ AlgorithmsTest.java
@@ -2,8 +2,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AlgorithmsTest {
 
+    Graph graph = new Graph();
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp(){
+
+        try {
+        graph.addNode("1");
+        graph.addNode("2");
+        graph.addNode("3");
+        graph.addNode("4");
+        graph.addNode("5");
+        graph.addNode("6");
+
+        graph.addLink(5,"1", "2");
+        graph.addLink(4,"2", "4");
+        graph.addLink(3,"4", "6");
+        graph.addLink(3,"1", "3");
+        graph.addLink(4,"3", "5");
+        graph.addLink(6,"5", "6");
+        graph.addLink(10,"1", "6");
+
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    //sets the two vectors used in the test to null
+    @org.junit.jupiter.api.AfterEach
+    void tearDown(){
+
+        try {
+
+            graph.removeNode("1");
+            graph.removeNode("2");
+            graph.removeNode("3");
+            graph.removeNode("4");
+            graph.removeNode("5");
+            graph.removeNode("6");
+
+            graph.removeLink("1", "2");
+            graph.removeLink("2", "4");
+            graph.removeLink("4", "6");
+            graph.removeLink("1", "3");
+            graph.removeLink("3", "5");
+            graph.removeLink("5", "6");
+            graph.removeLink("1", "6");
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
     @org.junit.jupiter.api.Test
     void floydWarshall() {
+
+
     }
 
     @org.junit.jupiter.api.Test
