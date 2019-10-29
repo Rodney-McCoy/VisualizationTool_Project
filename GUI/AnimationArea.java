b2af0ad09bbb2355cd8ae22bc73f114b047c15ce
NathanLervold <54128070+NathanLervold@users.noreply.github.com>
2019-10-29 14:54:10 -0600

    Add files via upload

--- GUI/AnimationArea.java
+++ GUI/AnimationArea.java
@@ -11,54 +11,123 @@ public class AnimationArea {
         g.fillOval(x, y, nodeDiameter,nodeDiameter);
     }
 
-    public void animate(Graphics g, DrawingPanel panel, Gui gui){
+    /**
+     * animate- loop that draws the graph
+     * @param g- graphics
+     * @param panel- panel where things are drawn
+     * @param gui- gui
+     * @param graph- graph that will be drawn on panel
+     */
+
+    public void animate(Graphics g, DrawingPanel panel, Gui gui, Graph graph){
         panel.setBackground(Color.WHITE);
 
         while(gui.exit != 1) {
             panel.copyGraphicsToScreen();
-            //panel.setBackground(Color.WHITE);
-            //draw Nodes
-            //drawEdges
+            panel.setBackground(Color.WHITE);
+            drawNodes(g, graph);
+            drawEdges(g, graph);
+
             try {
                 Thread.sleep(100);
-            } catch (Exception e) {}
+            } catch (Exception e) {
+                System.out.println("Error in AnimationArea while: " + e);
+            }
 
             if(gui.setting == 1) {
+                //create Node
                 if (panel.mouseClickHasOccurred(0)) {
-                    g.setColor(Color.RED);
-                    //createNode
-                    paintNode(g, panel.getMouseClickX(0) - nodeDiameter/2, panel.getMouseClickY(0) - nodeDiameter/2);
+                    int clickX = panel.getMouseClickX(0);
+                    int clickY = panel.getMouseClickY(0);
+
+                    //if cannot find a node in that location, add one
+                    if(findNode(clickX, clickY, graph) == -1) {
+                        graph.addNode(clickX, clickY);
+                    }
                 }
             }
 
             if(gui.setting == 2) {
+                //create Edge
+                //should: check click proximity to node, if in range make that node part of the edge
+                //        do this for second node
+                //        then print out the line from first node x,y and second node x,y
+                //Proof of Concept:
                 if (panel.mouseClickHasOccurred(0)) {
-                    g.setColor(Color.BLACK);
-                    //createEdge
                     int firstX = panel.getMouseClickX(0);
                     int firstY = panel.getMouseClickY(0);
-
+                    //findNode(firstX, firstY, graph);
                     while(!panel.mouseClickHasOccurred(0) && gui.setting == 2) {
                         try {
                             Thread.sleep(100);
-                        } catch (Exception e) {}
+                        } catch (Exception e) {
+                            System.out.println("Error in AnimationArea setting == 2: " + e);
+                        }
                     }
                     g.drawLine(firstX, firstY, panel.getMouseClickX(0), panel.getMouseClickY(0));
                 }
+
             }
 
             if(gui.setting == 3){
+                //Delete node or Edge
+                //should: Check mouse click proximity to node
+                //            if close, delete node
+                //        Then if not on node, check proximity to edge
+                //        Check normalized vector from node xy to mouse xy.  then check if magnitude is smaller
+                //            if angle is similiar and magnitude smaller, delete edge
+                //Proof of Concept:
                 if(panel.mouseClickHasOccurred(0)){
                     g.setColor(Color.WHITE);
-                    //deleteNode
-                    //deleteEde
                     paintNode(g, panel.getMouseClickX(0), panel.getMouseClickY(0));
                 }
             }
         }
     }
 
-    private void drawNodes(){
-
+    /**
+     * drawNodes- goes through all nodes and draws each node
+     * @param g- graphics
+     * @param graph- graph
+     */
+    private void drawNodes(Graphics g, Graph graph){
+        //iterate through every node in graph, draw to graphics
+        for(int i = 0; i < graph.getNumNodes(); ++i){
+            g.setColor(Color.RED);
+            g.fillOval(graph.nodes.get(i).xPos - this.nodeDiameter/2, graph.nodes.get(i).yPos - this.nodeDiameter/2, this.nodeDiameter, this.nodeDiameter);
+        }
     }
+
+    /**
+     * drawEdges- goes through all edges and draws each edge
+     * @param g- graphics
+     * @param graph- graph
+     */
+    private void drawEdges(Graphics g, Graph graph){
+        //iterate through every edge in graph, draw to graphics
+        //start x = node1, start y = node2
+        for(int i = 0; i < graph.getNumLinks(); ++i){
+            g.setColor(Color.BLACK);
+            g.drawLine(graph.links.get(i).getNode1().xPos, graph.links.get(i).getNode1().yPos, graph.links.get(i).getNode2().xPos,graph.links.get(i).getNode2().yPos);
+        }
+    }
+
+    /**
+     * findNode- sees if mouse click is on a node
+     * @param x- mouse click x
+     * @param y- mouse click y
+     * @param graph- graph
+     * @return - -1 if mouse is on no node, else returns node index
+     */
+    private int findNode(int x, int y, Graph graph){
+        for(int i = 0; i < graph.nodes.size(); ++i){
+            if(this.nodeDiameter > Math.abs(x - (graph.nodes.get(i).xPos))){
+                if(this.nodeDiameter > Math.abs(y - (graph.nodes.get(i).yPos))){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
